acf98d76a3a73cf829fa2990ff328ebcc077956b
Rosa Maggi <rosa.maggi@epfl.ch>
2026-09-25 17:45:06 +0200

    [feature] Add mutation for `addOrganism`

--- src/schema/types/bioOrg.ts
+++ src/schema/types/bioOrg.ts
@@ -2,6 +2,10 @@ import {builder} from "../builder";
 import {OptimisticLock} from "../../lib/optimisticLock";
 import {z} from "zod";
 import {buildSearchConditions} from "../../lib/searchConditionBuilder";
+import {fileNameRegexp, organismRegexp} from "../../lib/lhdValidators";
+import {sanitizeBase64DataUrl} from "../../lib/fieldValidatePlugin";
+import {getUserString} from "../../lib/userType";
+import {saveBase64File} from "../../lib/fileUtilities";
 
 const BioOrgRef = builder.prismaObject('BioOrg', {
 	name: 'BioOrg',
@@ -89,3 +93,54 @@ builder.queryField('organismsFromFullText', (t) =>
 		},
 	})
 );
+
+builder.mutationType({
+	fields: (t) => ({
+		addOrganism: t.string({
+			authScopes: {
+				needPermission: 'canEditOrganisms'
+			},
+			args: {
+				organismName: t.arg.string({required: true}),
+				risk: t.arg.int({required: true}),
+				fileContent: t.arg.string(),
+				fileName: t.arg.string(),
+			},
+			validate: z.object({
+				organismName: z.string().regex(organismRegexp),
+				risk: z.number().int().lte(3).gte(1),
+				fileContent: z.string().optional().refine(
+						(value) => value === undefined || sanitizeBase64DataUrl(value), {
+							message: 'Invalid file content',
+						}
+					),
+				fileName: z.string().regex(fileNameRegexp).optional(),
+			}),
+			resolve: async (root, args, ctx: any) => {
+				return await ctx.prisma.$transaction(async (tx: any) => {
+					const organism = await tx.BioOrg.create({
+						data: {
+							organism: args.organismName,
+							riskGroup: args.risk,
+							updatedOn: new Date(),
+							updatedBy: getUserString(ctx.user),
+						}
+					});
+
+					if (args.fileContent && args.fileName) {
+						await tx.BioOrg.update({
+							data: {
+								filePath: saveBase64File(args.fileContent, 'd_bio/' + organism.idBioOrg + '/', args.fileName)
+							},
+							where: {
+								idBioOrg: organism.idBioOrg
+							}
+						});
+					}
+
+					return organism.organism;
+				});
+			},
+		}),
+	}),
+});
