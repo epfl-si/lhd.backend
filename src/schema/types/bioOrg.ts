27782f642625d6720a76c7fa399ba4b9328aca06
Rosa Maggi <rosa.maggi@epfl.ch>
2026-09-25 14:12:53 +0200

    [feature] Add `organismsFromFullText` query

--- src/schema/types/bioOrg.ts
+++ src/schema/types/bioOrg.ts
@@ -1,14 +1,15 @@
 import {builder} from "../builder";
 import {OptimisticLock} from "../../lib/optimisticLock";
+import {z} from "zod";
 
-builder.prismaObject('BioOrg', {
+const BioOrgRef = builder.prismaObject('BioOrg', {
 	name: 'BioOrg',
 	fields: (t: any) => ({
 		organism: t.exposeString('organism'),
 		riskGroup: t.exposeInt('riskGroup'),
 		filePath: t.exposeString('filePath'),
 		updatedOn: t.expose('updatedOn', { type: 'DateTime' }),
-		updated_by: t.exposeString('updated_by'),
+		updatedBy: t.exposeString('updatedBy'),
 		opLock: t.field({
 			type: 'String',
 			resolve: async (parent: any, _: any, context: any) => {
@@ -27,6 +28,19 @@ export function getBioOrgToString(parent: any) {
 	};
 }
 
+const BioOrgListResult = builder.objectRef<{
+	totalCount: number;
+	bios: any[];
+}>('BioOrgListResult').implement({
+	fields: (t) => ({
+		totalCount: t.exposeInt('totalCount'),
+		bios: t.field({
+			type: [BioOrgRef],
+			resolve: (parent) => parent.bios,
+		}),
+	}),
+});
+
 builder.queryType({
 	fields: (t) => ({
 		bioOrgs: t.prismaField({
@@ -34,15 +48,43 @@ builder.queryType({
 			authScopes: {
 				needPermission: 'canListOrganisms'
 			},
-			args: {
-				barcode: t.arg.string(),
-			},
-			validate: z.object({
-				barcode: z.string().nonempty(),
-			}),
 			resolve: async (query, root, args, ctx: any, info) => {
 				return await ctx.prisma.BioOrg.findMany();
 			},
 		}),
 	}),
 });
+
+builder.queryField('organismsFromFullText', (t) =>
+	t.field({
+		type: BioOrgListResult,
+		args: {
+			search: t.arg.string(),
+			skip: t.arg.int({defaultValue: 0, required: true}),
+			take: t.arg.int({defaultValue: 20, required: true}),
+		},
+		validate: z.object({
+			search: z.string().optional(),
+			skip: z.number().int().nonnegative().optional(),
+			take: z.number().int().nonnegative().optional()
+		}),
+		authScopes: {
+			needPermission: 'canListOrganisms'
+		},
+		resolve: async (root, args, ctx: any) => {
+			const bioList =  await ctx.prisma.BioOrg.findMany({
+				where: { organism: { contains: args.search } },
+				orderBy: [
+					{
+						organism: 'asc',
+					},
+				]
+			});
+
+			const bios = args.take == 0 ? bioList : bioList.slice(args.skip, args.skip + args.take);
+			const totalCount = bioList.length;
+
+			return { bios, totalCount };
+		},
+	})
+);
